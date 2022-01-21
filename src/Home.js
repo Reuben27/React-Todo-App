24e61095ae3580349e0f1fa6073c16f1869a42c5
Reuben27 <reuben.sd@iitgn.ac.in>
2022-01-21 11:02:54 +0530

    add project

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import AddTask from './AddTask';
+import TaskList from "./TaskList";
+import useFetch from "./useFetch";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => {
+  const {data: tasks, isPending, error} = useFetch('http://localhost:8000/tasks');
+
+  return (
+    <div className = "container">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading....</div>}
+      {/*blogs is null intially so the below statement only renders when blogs data has been fetched!*/}
+      {/* <AddTask/>
+      <br></br> */}
+      <h3>Tasks</h3>
+      <div></div>
+      {tasks && <TaskList tasks = {tasks} type = "normal"/>}
+    </div>
+  );
+}
+ 
+export default Home;
